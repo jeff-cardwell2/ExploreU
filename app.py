2630deef448d114447cb13069c19bf2713f9045a
Andy Chen <andy27479@gmail.com>
2023-03-18 14:46:00 -0400

    setting up home layout

--- app.py
+++ app.py
@@ -17,7 +17,44 @@ app = Dash(
 server = app.server
 app.title = 'ExploreU'
 
-app.layout = html.Div([dash.page_container])
+header = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.Img(src=app.get_asset_url('icon.png'), height="25px")
+                        ),
+                        dbc.Col(
+                            dbc.NavbarBrand("ExploreU", class_name="ms-2"),
+                        )
+                    ],
+                    align="center",
+                    class_name="g-0",
+                ),
+                href=dash.page_registry['pages.home']['path'],
+                style={"textDecoration": "none", 'fontWeight':'bold'},
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        html.A('Code', href="https://github.com/jeff-cardwell2/SI699-Project", target="_blank"),
+                        width="auto",
+                        className="nav-link"
+                    )
+                ],
+                className="g-0",
+            )
+        ],
+        class_name="mx-1",
+        fluid=True
+    ),
+    color="dark",
+    dark=True,
+)
+
+app.layout = html.Div([header, dash.page_container])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- pages/home.py
+++ pages/home.py
@@ -5,4 +5,23 @@ from dash.dependencies import Input, Output, State
 
 dash.register_page(__name__, path='/')
 
-layout = dbc.Container()
+topics = ['Math', 'Linguistics', 'History', 'Politics', 'Gaming', 'Music']
+
+search = dbc.Row(
+    [
+        html.Br(),
+        dbc.Col(
+            [
+                html.Br(),
+                dcc.Dropdown(topics, value=[], persistence=True, multi=True, placeholder="Select topics in order", id="topics")
+            ],
+            class_name="g-2 ms-auto flex-wrap mx-auto",
+        )
+    ],
+    align="center",
+    justify="center"
+)
+
+layout = dbc.Container([
+    search
+])
