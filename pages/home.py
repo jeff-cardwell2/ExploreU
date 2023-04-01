c4c28dcb1679ca9cb136ea8448fabdd4f1feeb7e
Andy Chen <andy27479@gmail.com>
2023-04-01 10:06:26 -0400

    added cip info retrieval

--- pages/home.py
+++ pages/home.py
@@ -2,6 +2,7 @@ import dash
 from dash import html, dcc, dash_table as dt
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
+import pandas as pd
 
 dash.register_page(__name__, path='/', title="ExploreU")
 
@@ -9,12 +10,18 @@ topics = ['Math', 'Linguistics', 'History', 'Politics', 'Gaming', 'Music']
 
 search = dbc.Row(
     [
-        html.Br(),
         dbc.Col(
             [
-                html.Br(),
-                dcc.Dropdown(topics, value=[], persistence=True, multi=True, placeholder="Select topics in order", id="topics")
+                dcc.Dropdown(topics, value=[], persistence=True, multi=True, placeholder="Select topics in order", id="topics"),
+                dcc.Store(id='results-temp'),
             ]
+        ),
+        dbc.Col(
+            dbc.Button(
+                "Search", color="secondary", class_name="ms-2",
+                id="search-button", n_clicks=0
+            ),
+            width="auto"
         )
     ],
     class_name="g-2 ms-auto flex-wrap mx-auto",
@@ -23,18 +30,87 @@ search = dbc.Row(
     style={'width': '800px'}
 )
 
-results_container = dbc.Container(
+results_container = dbc.Col(
     [
-        dbc.Col()
+        html.P(id="cip-count"),
+        html.Div(id="cip-results")
     ]
 )
 
-viz_container = dbc.Container(
+viz_container = dbc.Col(
     [
-        dbc.Col()
+        html.Div(id="viz-container")
     ]
 )
 
 layout = dbc.Container([
-    search
+    html.Br(),
+    search,
+    html.Br(),
+    dbc.Row(
+        [
+            dbc.Col(results_container),
+            dbc.Col(viz_container)
+        ]
+    )
 ])
+
+@dash.callback(
+    [
+        Output('results-temp', 'data'),
+        Output('cip-count', 'children')
+    ],
+    Input('search-button', 'n_clicks'),
+    State('topics', 'value')
+)
+def update_results(click, topics):
+    # retrieve results from model here
+    cip_results = ["45.06", "30.70", "11.01", "13.06"]
+    cip_info = pd.read_csv("assets/data/cip_url_summary.csv", dtype={"CIPCode": str})
+
+    cips = list(cip_info['CIPCode'])
+    results = [{
+        "cip": cip,
+        "name": cip_info.loc[cip_info['CIPCode'] == cip, "CIPTitle"].item()[:-1],
+        "related": "Finance and Financial Management Services, Management Sciences and Quantitative Methods",
+        "description": cip_info.loc[cip_info['CIPCode'] == cip, "CIPDefinition_summary"].item(),
+        "url": cip_info.loc[cip_info['CIPCode'] == cip, "url"].item()
+    } for cip in cip_results if cip in cips]
+
+    n_results = f"Returning top {len(results)} matches"
+
+    return results, n_results
+
+@dash.callback(
+    Output('cip-results', 'children'),
+    Input('results-temp', 'data')
+)
+def display_cards(data):
+    cards = []
+    for cip in data:
+        cards.append(generate_card(cip))
+        cards.append(html.Br())
+
+    return cards
+
+def generate_card(data):
+    related = data['related'].split(', ')
+    card = dbc.Card(
+        [
+            dbc.CardHeader(html.A(f"CIP {data['cip']}", href=data['url'], target="_blank")),
+            dbc.CardBody(
+                [
+                    html.H4(data['name'], className="card-title"),
+                    html.P(data['description'], className="card-text"),
+                    html.Div(
+                        [
+                            html.Span(related[0]),
+                            html.Span(related[1])
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+    return card
