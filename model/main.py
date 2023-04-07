e2fe9d3e2a92a1550cf3a00bfc91b3d6da2d7c01
Andy Chen <andy27479@gmail.com>
2023-04-07 15:14:04 -0400

    set up model api and weighting

--- model/main.py
+++ model/main.py
@@ -1,34 +1,39 @@
 from fastapi import FastAPI
 import tensorflow as tf
+import tensorflow_ranking as tfr
+import tensorflow_recommenders as tfrs
 import pandas as pd
+import numpy as np
 
 app = FastAPI()
-model = tf.keras.models.load_model('mse')
-qrels = pd.read_csv('training_qrels_annotated.csv').drop(columns='cip_name')
-docset = pd.read_csv('final_curriculum_data/final_docset.csv').drop(index=[210, 199, 47, 190]).reset_index(drop=True)
-cip_titles = pd.read_csv('final_curriculum_data/cip_names.csv')[['Title', 'CIP Code']]
-cip_titles['CIP Code'] = [i[2:-1] if i[2] != '0' else i[3:-1] for i in cip_titles['CIP Code']]
-cip_titles['CIP Code'] = [i[:-1] if i[-1] == '0' else i for i in cip_titles['CIP Code']]
-docset = docset[docset['cip'].isin(cip_titles['CIP Code'])].reset_index(drop=True)
-docset['cip_name'] = [cip_titles[cip_titles['CIP Code']==i].Title.iloc[0] for i in docset.cip]
-docset['cip'] = docset['cip'].astype(str)
-qrels['courses'] = [r['courses'] for i in qrels['cip_code'] for ind, r in docset.iterrows() if str(i) == str(r['cip'])]
-qrels['descriptions'] = [r['descriptions'] for i in qrels['cip_code'] for ind, r in docset.iterrows() if str(i) == str(r['cip'])]
-
-all_queries = pd.read_csv('query_terms.csv')['0'].unique().tolist()
-all_courses = docset['courses'].astype(str).tolist()
-all_descriptions = docset['descriptions'].astype(str).tolist()
+model = tf.keras.models.load_model('pointwise_model')
 
 @app.get("/topics/{topics}")
 def get_cips(topics: str):
     topics_ls = topics.split("+")
-    cips = []
-    return {"cips": cips}
 
-def retrieve_cips(topic):
-    pass
+    cip_dict = {}
+    for i, topic in enumerate(topics_ls):
+        pred = generate_predictions(topic, model)
+        for j, cip in enumerate(pred):
+            cip_dict[cip] = cip_dict.get(cip, 0) + (1 / np.log2(2 + i)) * (1 / np.log2(2 + j))
 
-def generate_predictions(query, model):
+    top_cips = sorted(cip_dict, key=cip_dict.get, reverse=True)[:10]
+    
+    return {"cips": top_cips}
+
+def generate_predictions(query, model, n_cips=10):
+    docset = pd.read_csv('final_docset.csv').drop(index=[210, 199, 47, 190]).reset_index(drop=True)
+    cip_titles = pd.read_csv('cip_names.csv')[['Title', 'CIP Code']]
+    cip_titles['CIP Code'] = [i[2:-1] if i[2] != '0' else i[3:-1] for i in cip_titles['CIP Code']]
+    cip_titles['CIP Code'] = [i[:-1] if i[-1] == '0' else i for i in cip_titles['CIP Code']]
+    docset = docset[docset['cip'].isin(cip_titles['CIP Code'])].reset_index(drop=True)
+    docset['cip_name'] = [cip_titles[cip_titles['CIP Code']==i].Title.iloc[0] for i in docset.cip]
+    docset['cip'] = docset['cip'].astype(str)
+
+    all_queries = pd.read_csv('query_terms.csv')['0'].unique().tolist()
+    all_courses = docset['courses'].astype(str).tolist()
+    all_descriptions = docset['descriptions'].astype(str).tolist()
     prediction_dataset = tf.data.Dataset.from_tensor_slices({'query':[[query]],'courses':[[all_courses]], 'descriptions':[[all_descriptions]]})
     prediction_input = list(prediction_dataset.as_numpy_iterator())[0]
 
@@ -59,8 +64,6 @@ def generate_predictions(query, model):
     preds = model.score_model(concatenated_embeddings)
     scores = pd.DataFrame({'score':tf.squeeze(preds,-1).numpy()[0]})
     top_scores = scores.sort_values('score', ascending=False).head(20)
-    top_scores['query'] = [query]*20
-    top_scores['cip'] = [docset.iloc[i]['cip'] for i in top_scores.index]
-    top_scores['cip_name'] = [docset.iloc[i]['cip_name'] for i in top_scores.index]
+    cip_results = ['{:05.2f}'.format(float(docset.iloc[i]['cip'])) for i in top_scores.index]
 
-    return top_scores
+    return cip_results[:n_cips]
