85d35ef365ae73baa1d2e83c74e6a2f1a6bfdd1a
Andy Chen <andy27479@gmail.com>
2023-04-14 22:34:01 -0400

    added all four visualizations

--- pages/home.py
+++ pages/home.py
@@ -11,13 +11,16 @@ import plotly.graph_objects as go
 dash.register_page(__name__, path='/', title="ExploreU")
 
 topics_path = os.getcwd() + dash.get_asset_url("data/query_terms.csv")
-pdf_path = os.getcwd() + dash.get_asset_url("data/full_bach_pdf.csv")
+pdf_path = os.getcwd() + dash.get_asset_url("data/pdf_merged.csv")
 cip_path = os.getcwd() + dash.get_asset_url("data/cip_url_summary.csv")
+sc_path = os.getcwd() + dash.get_asset_url("data/scorecard_merged.csv")
 
 topics = list(pd.read_csv(topics_path)['0'])
-pdf_data = pd.read_csv(pdf_path, dtype={"cip_code": str, "year": int})
+pdf_data = pd.read_csv(pdf_path, index_col=0, dtype={'cip_code': str, 'CIPCode': str, "year": int})
 cip_info = pd.read_csv(cip_path, dtype={"CIPCode": str})
-cip_full = pdf_data.merge(cip_info[['CIPCode', 'CIPTitle']], how='right', left_on='cip_code', right_on='CIPCode')
+sc_data = pd.read_csv(sc_path, index_col=0, dtype={'cip_code': str, 'CIPCode': str})
+
+colors = ["#F8766D", "#D89000", "#A3A500", "#39B600", "#00BF7D", "#00BFC4", "#00B0F6", "#9590FF", "#E76BF3", "#FF62BC"]
 
 search = dbc.Row(
     [
@@ -76,7 +79,9 @@ viz_container = dbc.Col(
         html.Div(
             [
                 dcc.Graph(id="salary-viz"),
-                dcc.Graph(id="employment-viz")
+                dcc.Graph(id="employment-viz"),
+                dcc.Graph(id="nyear-viz"),
+                dcc.Graph(id="gender-viz")
             ],
             id="viz-container",
             style={'display': 'none'}
@@ -189,15 +194,52 @@ def generate_labels(data):
 )
 def plot_salary(data):
     cip_list = [i['cip'] for i in data]
-    fig_data = cip_full[cip_full['cip_code'].isin(cip_list)]
-    fig = px.line(
-        fig_data, x='year', y='mean_starting_salary', color='cip_code',
-        labels={'year': '', 'mean_starting_salary': '', 'cip_code': "CIP"}, template="ggplot2"
-    )
-    fig.update_traces(mode="markers+lines", hovertemplate=None)
-    fig.update_layout(hovermode="x unified", title={'text': "Mean Starting Salary", 'y': 0.95}, height=400)
+    fig_data = pdf_data[pdf_data['cip_code'].isin(cip_list)]
+    salary = go.Figure()
     
-    return fig
+    for i, cip in enumerate(cip_list):
+        cip_data = fig_data[fig_data['cip_code'] == cip].sort_values(by=['year'])
+        if len(cip_data) > 0:
+            cip_data['cip_upper'] = cip_data['mean_starting_salary'] + 500
+            cip_data['cip_lower'] = cip_data['mean_starting_salary'] - 500
+            cip_title = cip_data.iloc[0]['CIPTitle']
+            
+            year_bounds = list(cip_data['year']) + list(cip_data['year'])[::-1]
+            salary_bounds = list(cip_data['cip_upper']) + list(cip_data['cip_lower'])[::-1]
+            
+            salary.add_trace(
+                go.Scatter(
+                    name=cip_title,
+                    x=year_bounds,
+                    y=salary_bounds,
+                    mode='lines',
+                    hoverinfo='name',#"%{name}<extra></extra>",
+                    fill='toself',
+                    fillcolor=colors[i],
+                    line_color=colors[i],
+                    showlegend=False,
+                    opacity=0.25
+                )
+            )
+            
+            salary.add_trace(
+                go.Scatter(
+                    name=cip,
+                    x=cip_data['year'],
+                    y=cip_data['mean_starting_salary'].round(-3),
+                    mode='lines+markers',
+                    line_color=colors[i],
+                    hovertext=cip_data['CIPTitle'],
+                    hovertemplate="<b>%{hovertext}</b><br><br>"+"Year: %{x}<br>"+"Mean Starting Salary: $%{y}<br>"+"<extra></extra>"
+                )
+            )
+    
+    salary.update_layout(
+        template='ggplot2', legend_title='CIP Code', hoverlabel=dict(namelength=-1),
+        title={'text': "Mean Starting Salary", 'y': 0.85}, height=400
+    )
+    
+    return salary
 
 
 @dash.callback(
@@ -207,40 +249,43 @@ def plot_salary(data):
 )
 def plot_employment(data):
     cip_list = [i['cip'] for i in data]
-    fig_data = cip_full[cip_full['cip_code'].isin(cip_list)]
-    grouped = fig_data.groupby(['cip_code', 'CIPTitle']).mean(numeric_only=True).reset_index()
+    fig_data = pdf_data[pdf_data['cip_code'].isin(cip_list)]
+    grouped = fig_data.groupby('cip_code').mean(numeric_only=True).reset_index()
+    grouped = grouped.merge(fig_data[['CIPCode', 'CIPTitle']], how='left', left_on='cip_code', right_on='CIPCode')
+    grouped.drop_duplicates(inplace=True)
+    
     # selected_grouped = grouped[grouped['cip_code'].isin(selected_cips)]
     # other_grouped = grouped[~grouped['cip_code'].isin(selected_cips)]
-    
     # if len(selected_grouped) == 0:
     #     opacity = 1.0
     # else:
     #     opacity = 0.25
     
-    fig = go.Figure(
+    stacked = go.Figure(
         data=[
             go.Bar(
                 name='Percent Employed',
                 x=grouped['cip_code'],
                 y=grouped['total_perc_employed_overall'],
-                hovertext = grouped['CIPTitle'],
-                hovertemplate = "<b>%{hovertext}</b><br><br>" + "Percent Employed: %{y:.2f}%<br>"+"<extra></extra>"
-            ),                    
+                marker_color=colors[4],
+                hovertext=grouped['CIPTitle'],
+                hovertemplate="<b>%{hovertext}</b><br><br>"+"Percent Employed: %{y:.0f}%<br>"+"<extra></extra>"
+            ),                     
             go.Bar(
                 name='Percent Continuing Education',
                 x=grouped['cip_code'],
                 y=grouped['perc_continuing_ed'],
-                hovertext = grouped['CIPTitle'],
-                hovertemplate = "Percent Continuing Education: %{y:.2f}%<br>"+"<extra></extra>"
+                marker_color=colors[6],
+                hovertext=grouped['CIPTitle'],
+                hovertemplate="Percent Continuing Education: %{y:.0f}%<br>"+"<extra></extra>"
             ),
-                                
             #   go.Bar(name='Percent Employed',
             #         x=other_grouped['cip_code'],
             #         y=other_grouped['total_perc_employed_overall'],
             #         opacity = opacity,
             #         hovertext = other_grouped['CIPTitle'],
             #         hovertemplate = "<b>%{hovertext}</b><br><br>" +
-            #          "Percent Employed: %{y:.2f}%<br>"+"<extra></extra>"),
+            #          "Percent Employed: %{y:.0f}%<br>"+"<extra></extra>"),
                 
             #   go.Bar(name='Percent Continuing Education',
             #         x=other_grouped['cip_code'],
@@ -248,18 +293,138 @@ def plot_employment(data):
             #         opacity = opacity,
             #         hovertext = other_grouped['CIPTitle'],
             #         hovertemplate =  
-            #          "Percent Continuing Education: %{y:.2f}%<br>"+"<extra></extra>")
+            #          "Percent Continuing Education: %{y:.0f}%<br>"+"<extra></extra>")
         ]
     )
-
-    fig.update_layout(
-        barmode='stack',
-        title='Average First-Year Outcomes by CIP Code',
-        xaxis_title = 'CIP Code',
-        yaxis_title = 'Percentage of First-Year Graduates',
-        hovermode = 'x unified',
-        template = 'ggplot2',
-        legend={'traceorder':'normal'}
+    
+    stacked.update_layout(
+        barmode='stack', title='Average First-Year Outcomes by CIP Code',
+        xaxis_title='CIP Code', yaxis_title='Percentage',
+        hovermode='x unified', template='ggplot2', height=400,
+        legend={'traceorder':'normal', 'borderwidth':1, 'yanchor':'middle', 'y':1.02, 'xanchor':'right', 'x':0.99}
     )
+    
+    return stacked
 
-    return fig
+
+@dash.callback(
+    Output('nyear-viz', 'figure'),
+    Input('results-temp', 'data'),
+    prevent_initial_call=True
+)
+def plot_nyear(data):
+    cip_list = [i['cip'] for i in data]
+    fig_data = sc_data[sc_data['cip_code'].isin(cip_list)]
+    # selected_grouped = fig_data[fig_data['cip_code'].isin(selected_cips)]
+    # other_grouped = fig_data[~fig_data['cip_code'].isin(selected_cips)]
+    # if len(selected_grouped) == 0:
+    #     opacity = 1.0
+    # else:
+    #     opacity = 0.25
+
+    grouped = go.Figure(
+        data=[
+            go.Bar(
+                name='1-Year Median Earnings',
+                x=fig_data['cip_code'],
+                y=fig_data['earnings.highest.1_yr.overall_median_earnings'].round(-3),
+                marker_color=colors[4],
+                hovertext=fig_data['CIPTitle'],
+                hovertemplate="<b>%{hovertext}</b><br><br>"+
+                    "1-Year Median Earnings: $%{y}<br>"+"<extra></extra>"
+            ),                   
+            go.Bar(
+                name='2-Year Median Earnings',
+                x=fig_data['cip_code'],
+                y=fig_data['earnings.highest.2_yr.overall_median_earnings'].round(-3),
+                marker_color=colors[6],
+                hovertext=fig_data['CIPTitle'],
+                hovertemplate="<b>%{hovertext}</b><br><br>"+
+                "2-year Median Earnings: $%{y}<br>"+"<extra></extra>"
+            ),
+                              
+            #   go.Bar(name='1-Year Median Earnings',
+            #         x=other_grouped['cip_code'],
+            #         y=other_grouped['earnings.highest.1_yr.overall_median_earnings'].round(-3),
+            #         opacity = opacity,
+            #         hovertext = other_grouped['CIPTitle'],
+            #         hovertemplate = "<b>%{hovertext}</b><br><br>" +
+            #          "1-Year Median Earnings: $%{y}<br>"+"<extra></extra>"),
+                
+            #   go.Bar(name='2-Year Median Earnings',
+            #         x=other_grouped['cip_code'],
+            #         y=other_grouped['earnings.highest.2_yr.overall_median_earnings'].round(-3),
+            #         opacity = opacity,
+            #         hovertext = other_grouped['CIPTitle'],
+            #         hovertemplate = "<b>%{hovertext}</b><br><br>" +
+            #          "2-Year Median Earnings: $%{y}<br>"+"<extra></extra>")
+        ]
+    )
+    grouped.update_layout(
+        barmode='group', title='First and Second Year Post-Grad Median Earnings by CIP Code',
+        xaxis_title='CIP Code', yaxis_title='Reported Post-Grad Earnings',
+        template='ggplot2', height=400,
+        legend={'traceorder':'normal', 'borderwidth':1, 'yanchor':'middle', 'y':1.02, 'xanchor':'right', 'x':0.99})
+    
+    return grouped
+
+
+@dash.callback(
+    Output('gender-viz', 'figure'),
+    Input('results-temp', 'data'),
+    prevent_initial_call=True
+)
+def plot_gender(data):
+    cip_list = [i['cip'] for i in data]
+    fig_data = sc_data[sc_data['cip_code'].isin(cip_list)]
+    
+    # selected_grouped = fig_data[fig_data['cip_code'].isin(selected_cips)]
+    # other_grouped = fig_data[~fig_data['cip_code'].isin(selected_cips)]
+    # if len(selected_grouped) == 0:
+    #     opacity = 1.0
+    # else:
+    #     opacity = 0.25
+    
+    grouped = go.Figure(
+        data=[
+            go.Bar(
+                name='1-Year Nonmale Median Earnings',
+                x=fig_data['CIPCode'],
+                y=fig_data['earnings.highest.1_yr.nonmale_median_earnings'].round(-3),
+                marker_color=colors[4],
+                hovertext=fig_data['CIPTitle'],
+                hovertemplate="<b>%{hovertext}</b><br><br>"+"Median Nonmale Earnings: $%{y}<br>"+"<extra></extra>"
+            ),                  
+            go.Bar(
+                name='1-Year Male Median Earnings',
+                x=fig_data['CIPCode'],
+                y=fig_data['earnings.highest.1_yr.male_median_earnings'].round(-3),
+                marker_color=colors[6],
+                hovertext=fig_data['CIPTitle'],
+                hovertemplate="<b>%{hovertext}</b><br><br>"+"Median Male Earnings: $%{y}<br>"+"<extra></extra>"
+            ),
+
+            #   go.Bar(name='1-Year Nonmale Median Earnings',
+            #         x=other_grouped['cip_code'],
+            #         y=other_grouped['earnings.highest.1_yr.nonmale_median_earnings'].round(-3),
+            #         opacity = opacity,
+            #         hovertext = other_grouped['CIPTitle'],
+            #         hovertemplate = "<b>%{hovertext}</b><br><br>" +
+            #          "Median Nonmale Earnings: $%{y}<br>"+"<extra></extra>"),
+                
+            #   go.Bar(name='1-Year Male Median Earnings',
+            #         x=other_grouped['cip_code'],
+            #         y=other_grouped['earnings.highest.1_yr.male_median_earnings'].round(-3),
+            #         opacity = opacity,
+            #         hovertext = other_grouped['CIPTitle'],
+            #         hovertemplate = "<b>%{hovertext}</b><br><br>" +
+            #          "Median Male Earnings: $%{y}<br>"+"<extra></extra>")
+        ]
+    )
+    grouped.update_layout(barmode='group', title='First Year Post-Grad Median Earnings (Male and Nonmale Graduates)',
+                          xaxis_title='CIP Code',
+                          yaxis_title='Reported Post-Grad Earnings',
+                          template='ggplot2',
+                          legend={'traceorder':'normal', 'borderwidth':1, 'yanchor':'middle', 'y':1.02, 'xanchor':'right', 'x':0.99})
+    
+    return grouped
